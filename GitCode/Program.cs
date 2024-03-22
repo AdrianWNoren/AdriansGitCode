ae9ece7dc197a88063f8c8d9d7e850a9a573a6b6
Adrian Norén <adrian.noren2@learnet.se>
2024-03-22 09:11:32 +0100

    first commit: upload project

---
+++ GitCode/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace GitCode
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till den här appen!");
+            int run = 0;
+            while (true)
+            {
+                Console.WriteLine("Välj ett heltal i menyn");
+                run = int.Parse(Console.ReadLine());
+                if (run == 1)
+                {
+                    Greetings();
+                }
+                else if (run == 2)
+                {
+                    GreetingsUser();
+                }
+                else 
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+        }
+
+        static void Greetings()
+        {
+            Console.WriteLine("Hello from GitCode");
+
+        }
+
+        static void GreetingsUser()
+        {
+            Console.WriteLine("Hello from Adrian");
+
+        }
+    }
+}
